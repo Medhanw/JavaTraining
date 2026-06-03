bffbb379754f9a1e0029ba2bab01e9f14ac99600
Medhanw <medha.wadekar05@gmail.com>
2026-06-03 11:50:07 +0530

    Create LoginLock.java

---
+++ LoginLock.java
@@ -0,0 +1,23 @@
+package JavaTraining;
+
+public class LoginLock {
+   public static void main(String[] args) {
+      String[] var1 = new String[]{"wrong", "wrong", "wrong"};
+      int var2 = 0;
+
+      for(String var6 : var1) {
+         if (!var6.equals("wrong")) {
+            System.out.println("Login Successful");
+            return;
+         }
+
+         ++var2;
+         if (var2 == 3) {
+            System.out.println("Account Locked");
+            return;
+         }
+      }
+
+   }
+}
+
