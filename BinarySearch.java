d42c3656d185bc721662ccac5d5bbfaee1f8268e
Medhanw <medha.wadekar05@gmail.com>
2026-06-04 10:32:57 +0530

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,23 @@
+package JavaTraining;
+
+public class BinarySearch {
+    public static void main(String[] args){
+        int[] arr = {10, 20, 30, 40, 45, 50};
+        int key = 20;
+        int l = 0, r = 0;
+        while(l<=r){
+            int mid = l + (r-1) / 2;
+            if(arr[mid] == key){
+                System.out.println("Index : " + mid);
+                return;
+            }
+            else if(arr[mid] < key){
+                l=mid+1;
+            }else{
+                r = mid - 1;
+            }
+        }
+        System.out.println("Element not found");
+    }
+    
+}
