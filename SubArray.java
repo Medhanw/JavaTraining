1726a2136ab9d1f5b240aa3c8a316eb5f8462107
Medhanw <medha.wadekar05@gmail.com>
2026-06-04 12:04:57 +0530

    Create SubArray.java

---
+++ SubArray.java
@@ -0,0 +1,23 @@
+package JavaTraining;
+
+public class SubArray {
+    public static void main(String[] args){
+        int[] arr = {1, 2, 1, 0, 1, 0, 3};
+        int k = 4;
+
+        int l = 0, max = 0, sum = 0;
+        for(int r=0; r<arr.length; r++){
+            sum += arr[r];
+
+            while(sum>k){
+                sum -= arr[l];
+                l++;
+            }
+            if(max<=k){
+                max = max<(r-l+1) ? r-l+1:max;
+            }
+        }
+        System.out.println(max);
+    }
+    
+}
