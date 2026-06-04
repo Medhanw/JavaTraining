85964b208dfefee6e467b24a6df1e2c36dcf3694
Medhanw <medha.wadekar05@gmail.com>
2026-06-04 10:33:02 +0530

    Create MinMax.java

---
+++ MinMax.java
@@ -0,0 +1,32 @@
+package JavaTraining;
+public class MinMax {
+    public static void main(String[] args) {
+        int[] arr = { 33, 10, 7, 25, 49, 55, 41, 6 };
+        int max = arr[0], smax = arr[0];
+        int min = Integer.MAX_VALUE, smin = Integer.MAX_VALUE, tmin = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                smax = max;
+                max = arr[i];
+            } else if (arr[i] > smax) {
+                smax = arr[i];
+            }
+
+            if (arr[i] < min) {
+                tmin = smin;
+                smin = min;
+                min = arr[i];
+            } else if (arr[i] < smin) {
+                tmin = smin;
+                smin = arr[i];
+            } else if (arr[i] < tmin) {
+                tmin = arr[i];
+            }
+
+        }
+        System.out.println(smax);
+        System.out.println(tmin);
+    }
+}
