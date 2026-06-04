d39fbef1ea6df4085bf38c99f5d395d1d5fbc4cd
Medhanw <medha.wadekar05@gmail.com>
2026-06-04 10:33:43 +0530

    Create TrafficFineSystem.java

---
+++ TrafficFineSystem.java
@@ -0,0 +1,25 @@
+package JavaTraining;
+import java.util.Scanner;
+public class TrafficFineSystem {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int speed = sc.nextInt();
+        boolean repeated = sc.nextBoolean();
+        int fine = 0;
+
+        if(speed > 100){
+            fine = 1000;
+        }
+        else if(speed > 80){
+            speed = 500;
+        }
+
+        if(repeated){
+            fine = fine * 2;
+        }
+
+        System.out.println(fine);
+        sc.close();
+    }
+    
+}
