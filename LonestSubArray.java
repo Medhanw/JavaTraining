d9a75a42cf800839f354b370c44725d0860fb516
Medhanw <medha.wadekar05@gmail.com>
2026-06-05 13:21:29 +0530

    Create LonestSubArray.java

---
+++ LonestSubArray.java
@@ -0,0 +1,21 @@
+
+import java.util.HashMap;
+public class LonestSubArray {
+    public static void main(String[] args) {
+        int[] arr = new int[] {3, -2, 5, -1, 2, -3, 6, -2, 4};
+        int k = 7;
+        HashMap<Integer, Integer > map = new HashMap<>();
+        map.put(0,1);
+        int sum = 0;
+        int count = 0;
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
+            if(map.containsKey(sum-k)){
+                count += map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0)+1);
+        }
+        System.out.println("Total subarrays with sum " + k + " is: " + count);
+    }
+}
+
