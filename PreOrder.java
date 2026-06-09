7214f7ec94c3bda3025f96253ab64f742302e7b0
Medhanw <medha.wadekar05@gmail.com>
2026-06-09 10:58:40 +0530

    Update PreOrder.java

--- PreOrder.java
+++ PreOrder.java
@@ -56,8 +56,10 @@ class TreeOps {
     int height(Node root){
         if(root == null) return 0;
         return 1 + Math.max(height(root.left), height(root.right));
-
-
+    }
+    int diameter = 0;
+    int getdiameter(Node root){
+        
     }
 }
 
