f501b34a714f8e434661eeb420f9b19f0b6a1a86
Medhanw <medha.wadekar05@gmail.com>
2026-06-10 12:04:23 +0530

    Update Graph1.java

--- Graph1.java
+++ Graph1.java
@@ -1,3 +1,4 @@
+import java.lang.classfile.constantpool.IntegerEntry;
 import java.util.*;
 import java.util.Queue;
 public class Graph1 {
@@ -13,15 +14,17 @@ public class Graph1 {
         graph.get(src).add(dest);
         graph.get(dest).add(src);
     }
-    static void BFS(int v){
+    static void BFS(int src, int v){
         boolean[] visited = new boolean[v];
-        Queue<Integer> que = new Queue<Integer>();
+        Queue<Integer> que = new LinkedList<>();
 
         que.add(src);
         visited[src] = true;
+        
         while(!que.isEmpty()){
             int data = que.poll();
             System.out.print(data + " ");
+            
             for(int neighbour : graph.get(data)){
                 if(!visited[neighbour]){
                     que.add(neighbour);
@@ -29,15 +32,62 @@ public class Graph1 {
                 }
             }
         }
-
     }
     static void DFS(int src, int v){
         Deque<Integer> st = new ArrayDeque<>();
-        boolean[] visited = new boolean[];
-
+        boolean[] visited = new boolean[v];
+        
+        st.push(src);
+        
+        while (!st.isEmpty()) {
+            int data = st.pop(); // Pop the node first
+            
+            if (!visited[data]) {
+                visited[data] = true;
+                System.out.print(data + " ");
+                ArrayList<Integer> neighbors = graph.get(data);
+                for (int i = neighbors.size() - 1; i >= 0; i--) {
+                    int neighbor = neighbors.get(i);
+                    if (!visited[neighbor]) {
+                        st.push(neighbor);
+            }
+            
+        }
     }
+}
+}
+    static void deleteNode(int targetNode, int totalVertices){
+        if(targetNode < 0 || targetNode >=graph.size()){
+            System.out.println("Node "+targetNode+"does not exist");
+            return;
+        }
+
+        for(int i =0; i<graph.size(); i++){
+            ArrayList<Integer> neighbors = graph.get(i); 
+            neighbors.remove((Integer) targetNode);
+        }
+        graph.get(targetNode).clear();
+        System.out.println("Node "+targetNode+"and its connections have been successfully deleted.");
+    }
+}
     public static void main(String[] args) {
-        createGraph(10);     
+        createGraph(6);
+        addEdge(0, 1);
+        addEdge(0, 5);
+        addEdge(1, 2);
+        addEdge(1, 5);
+        addEdge(2, 3);
+        addEdge(3, 4);
+        addEdge(4, 5);
+
+        System.out.print("DFS Traversal: ");
+        DFS(0, 6);     
+        System.out.println();
+
+        System.out.print("BFS Traversal: ");
+        BFS(0, 6);
+        System.out.println();
+
     }
 
 }
